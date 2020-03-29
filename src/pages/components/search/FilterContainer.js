1ad3a17b0871cb5ff7fc644254e28728786b9acf
Tabea Blenk <tabeablenk@gmail.com>
2020-03-29 21:38:00 +0200

    Added reset search option

--- src/pages/components/search/FilterContainer.js
+++ src/pages/components/search/FilterContainer.js
@@ -3,8 +3,12 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 import '../../css/search/filterContainer.css';
-import Button from "react-bootstrap/Button";
 
+let selectedStyle = {
+    transform: 'scale(1.10)',
+};
+
+let defaultStyle;
 
 export default class FilterContainer extends Component {
 
@@ -17,18 +21,25 @@ export default class FilterContainer extends Component {
     };
 
     handleClick(e) {
-        this.setState({
-            selectedFilter: e.currentTarget.id
-        });
-        this.props.onChange(e.currentTarget.id);
+        //toggle selection
+        if (this.state.selectedFilter === e.currentTarget.id) {
+            this.setState({
+                selectedFilter: 'none'
+            });
+            this.props.onChange('none');
+        } else {
+            this.setState({
+                selectedFilter: e.currentTarget.id
+            });
+            this.props.onChange(e.currentTarget.id);
+        }
     };
 
     render() {
-
         return (
             <Row className="filterRow">
                 <Col>
-                    <div onClick={this.handleClick} id="cafe" key="key" className="filterContainer cafeFilterContainer">
+                    <div style={this.state.selectedFilter === 'cafe' ? selectedStyle : defaultStyle} onClick={this.handleClick} id="cafe" key="key" className="filterContainer cafeFilterContainer">
                         <div className="filterContentHolder">
                             <img src="/assets/icons/cafe.svg" alt={"Cafe-Icon"}/>
                             <p>CAFÈS</p>
@@ -36,7 +47,7 @@ export default class FilterContainer extends Component {
                     </div>
                 </Col>
                 <Col>
-                    <div onClick={this.handleClick} id="restaurant"
+                    <div style={this.state.selectedFilter === 'restaurant' ? selectedStyle : defaultStyle} onClick={this.handleClick} id="restaurant"
                          className="filterContainer restaurantFilterContainer">
                         <div className="filterContentHolder">
                             <img src="/assets/icons/restaurant.svg" alt={"Restaurant-Icon"}/>
@@ -45,7 +56,7 @@ export default class FilterContainer extends Component {
                     </div>
                 </Col>
                 <Col>
-                    <div onClick={this.handleClick} id="baecker" className="filterContainer barFilterContainer">
+                    <div style={this.state.selectedFilter === 'baecker' ? selectedStyle : defaultStyle} onClick={this.handleClick} id="baecker" className="filterContainer barFilterContainer">
                         <div className="filterContentHolder">
                             <img src="/assets/icons/baecker.svg" alt={"Baecker-Icon"}/>
                             <p>BÄCKER</p>
@@ -53,7 +64,7 @@ export default class FilterContainer extends Component {
                     </div>
                 </Col>
                 <Col>
-                    <div onClick={this.handleClick} id="bar" className="filterContainer cocktailFilterContainer">
+                    <div style= {this.state.selectedFilter === 'bar' ? selectedStyle : defaultStyle} onClick={this.handleClick} id="bar" className="filterContainer cocktailFilterContainer">
                         <div className="filterContentHolder">
                             <img src="/assets/icons/cocktail.svg" alt={"Bar-Icon"}/>
                             <p>BARS</p>
