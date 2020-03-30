a12747a65747f3a86f4d71453415171945d14e4d
christopher@familie-voit.de <christopher@familie-voit.de>
2020-03-31 01:29:56 +0200

    password reset fix

--- src/pages/components/profile/PasswordChangeForm.js
+++ src/pages/components/profile/PasswordChangeForm.js
@@ -256,8 +256,8 @@ export default class PasswordChangeForm extends React.Component {
                             onSubmit={(values, {resetForm}) => {
                                 axios.post(process.env.REACT_APP_API_URL + '/api/users/setPassword', {
                                     user: {
-                                        password: values.oldPassword,
-                                        passwordConfirm: values.password
+                                        password: values.password,
+                                        passwordConfirm: values.passwordConfirm
                                     }
                                 }, {
                                     withCredentials: true
