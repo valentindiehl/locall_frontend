589b9999f36c38af7857920f62d7681076ec8d24
christopher@familie-voit.de <christopher@familie-voit.de>
2020-03-30 17:31:49 +0200

    minor changes to routing

--- src/App.js
+++ src/App.js
@@ -35,17 +35,14 @@ export default class App extends Component {
                     <Route path="/" exact component={LoginPage}/>
                     <Route path="/login" component={LoginPage}/>
                     <Route path="/logout" component={LoginPage}/>
-                    <Route path="/imprint" component={ImprintContainer}/>
-                    <Route path="/privacy-policy" component={PrivacyPolicyContainer}/>
-                    <Route path="/app" component={withAuth(Map)}/>
                     <Route path="/verify-email/:token" component={EmailVerification}/>
                     <Route path="/verify-application/:token" component={ApplicationVerification}/>
-                    <Route path="/onboarding" component={BusinessOnboardingPage}/>
                     <Route path="/reset-password/:token" component={PasswordResetPage}/>
+                    <Route path="/onboarding" component={BusinessOnboardingPage}/>
+                    <Route path="/app" component={withAuth(Map)}/>
                     <Route path="/profile" component={withAuth(ProfilePage)}/>
-                    <Route path="/business-profile" component={withAuth(GastroProfileContainer)}/>
-                    <Route path="/user-profile" component={withAuth(UserProfileContainer)}/>
-                    <Route path="/password-reset" component={PasswordResetPage}/>
+                    <Route path="/imprint" component={ImprintContainer}/>
+                    <Route path="/privacy-policy" component={PrivacyPolicyContainer}/>
                     <Route component={LoginPage}/>
                 </Switch>
             </Router>
