15a73b001ccd4dd9d840afef9f175b5baf1a8afc
Tabea Blenk <tabeablenk@gmail.com>
2020-03-30 21:15:15 +0200

    changes support button color to dark brown

--- src/pages/components/profile/SupportContainer.js
+++ src/pages/components/profile/SupportContainer.js
@@ -14,7 +14,7 @@ export default class SupportContainer extends Component {
         return (
             <div className="settings-container support">
                 <div className="box-heading">
-                    <img className="iconProfile" src={"/assets/icons/icons-mail.svg"} alt={"Mail-Icon"}/>
+                    <img className="iconProfile" src={"/assets/icons/icons-mail-dark.svg"} alt={"Mail-Icon"}/>
                     <h4>Support</h4>
                 </div>
                 <div className="white-box">
