c9c2aff3ca7e18e7e84efc77e2d6cc0073060f70
Jonas Müller <jonas.mueller@compitencies.com>
2020-04-12 10:36:14 +0200

    No mobile rendering for map

--- src/pages/components/registration/NoMobileRenderer.js
+++ src/pages/components/registration/NoMobileRenderer.js
@@ -1,15 +1,16 @@
 import React from "react";
+import {Container} from "react-bootstrap";
 
 const NoMobileRenderer = () => {
 	return (
-		<>
+		<Container style={{marginTop: 100}}>
 			<h4>Du möchtest mitmachen?</h4>
-			<p>Leider ist unsere Seite im Moment nur auf dem Desktop benutzbar. Setz dich doch einfach schnell
+			<p>Leider ist unsere Kartenansicht im Moment nur auf dem Desktop benutzbar. Setz dich doch einfach schnell
 				an deinen Laptop oder PC und melde dich dort an. In der Zwischenzeit arbeiten wir natürlich auf
 				Hochtouren an einer mobilen Version.<br/> Danke für deine Geduld!
 				<span role="img" aria-label="yellow-heart">💛</span><br/><br/>
 			</p>
-		</>
+		</Container>
 	)
 }
 
