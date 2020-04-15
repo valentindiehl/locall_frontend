82b0d1f15287bbaca3f17d19454e19eddef67646
Jonas Müller <jonas.mueller@compitencies.com>
2020-04-15 18:27:04 +0200

    Some styles and adjustments

--- src/pages/components/livestream/LiveStreamPageRenderer.js
+++ src/pages/components/livestream/LiveStreamPageRenderer.js
@@ -4,9 +4,15 @@ import LeftContainer from "./LeftContainer";
 import ChatContainer from "./chat/ChatContainer";
 import React from "react";
 
-const LiveStreamPageRenderer = () => {
+const LiveStreamPageRenderer = (props) => {
 	return (
 		<Container fluid>
+			<Row>
+				<Col xs={12} onClick={props.onClick} className={"backToMap"}>
+					<img src={"/assets/icons/back-arrow-dark.svg"} alt={"Left Arrow"}/>
+					<span>Zurück zur Karte</span>
+				</Col>
+			</Row>
 			<Row>
 				<Col lg={8}>
 					<LeftContainer/>
